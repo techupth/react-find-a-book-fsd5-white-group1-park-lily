453f7ffc13ab8b98519f6737b64558c145b6de63
Park <tanagrit.sornchaiyagorn@gmail.com>
2024-01-03 15:24:58 +0700

    featured: add search book from text feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import "./App.css";
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [listBook, setListBook] = useState([]);
+  const handleSearch = async (event) => {
+    let searchText = event.target.value;
+    const getListBook = await axios.get(
+      "https://www.googleapis.com/books/v1/volumes?q=" + searchText
+    );
+
+    setListBook(getListBook.data.items);
+  };
+
+  useEffect(() => {}, [listBook]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input type="string" onChange={handleSearch}></input>
+      <ul>
+        {listBook.map((items) => {
+          return <li key={items.id}>{items.volumeInfo.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
